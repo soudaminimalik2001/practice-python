65299a894e0b7bc9d08b10e05ba26a18745f6c5e
Soudamini <96462870+soudaminimalik2001@users.noreply.github.com>
2022-05-10 23:04:55 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,8 @@
+
+
+
+i=0
+while i<10:
+    # print(i,end="")
+    # i+=1
+    pass
---
+++ p3.py
@@ -0,0 +1,57 @@
+# a="soudamini"
+# i=0
+# c=0
+# c1=0
+# c2=0
+# c3=0
+# c4=0
+# c5=0
+# c6=0
+# c7=0
+# c8=0
+# c9=0
+# while i<len(a):
+#     # print(a[i])
+#     if a[i]=="s":
+#         c+=1
+#     if a[i]=="o":
+#         c1+=1
+#     if a[i]=="u":
+#         c2+=1
+#     if a[i]=="d":
+#         c3+=1
+#     if a[i]=="a":
+#         c4+=1
+#     if a[i]=="m":
+#         c5+=1
+#     if a[i]=="i":
+#         c6+=1
+#     if a[i]=="n":
+#         c7+=1
+#     if a[i]=="i":
+#         c8+=1
+#     i+=1
+# print("s",c)
+# print("o",c1)
+# print("u",c2)
+# print("d",c3)
+# print("a",c4)
+# print("m",c5)
+# print("i",c6)
+# print("n",c7)
+# print("i",c8)
+
+# a="soudamini"
+# i=0
+# c=0
+# while i<len(a):
+#     b=a[i]
+#     if a[i]==b:
+#         c+=1
+#     i+=1
+#     print(c)
+    
+a=[1,2,3,4,5]
+print(a)
+b={"one":1,"two":2}
+print(b)
